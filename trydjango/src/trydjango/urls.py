e717b126f4ee216c1be8c2e0d809dedf643f21d1
Saswat Levin <saswatlevin@gmail.com>
2019-07-07 18:15:18 +0300

    Corrected minor errors. views.py is the place where we create pages. Each app has its own views.py. The url for each view is then specified in urls.py under trydjango. Also created and registered an app 'pages' under INSTALLED_APPS in settings.py

--- trydjango/src/trydjango/urls.py
+++ trydjango/src/trydjango/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
+    path('admin/', admin.site.urls)
+
 ]
