7537c1e70cb61f9ecf5decc8f771bd9a48f04823
Saswat Levin <saswatlevin@gmail.com>
2019-07-08 09:02:29 +0300

    added for loop and changed views.py

--- trydjango/src/pages/views.py
+++ trydjango/src/pages/views.py
@@ -10,7 +10,14 @@ def contact_view(request,*args,**kwargs):
    return render(request,"contact.html",{})
 
 def about_view(request,*args,**kwargs):
-   return render(request,"about.html",{})
+   my_context={
+   "my_text":"This is about us",
+    "my_number":123,
+    "my_list":[123,4242,319]
+   }
+   
+ 
+   return render(request,"about.html",my_context)
 
 def social_view(request,*args,**kwargs):
    return render(request,"social.html",{})
