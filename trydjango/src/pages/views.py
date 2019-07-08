40946d56f506161725e4da0153fde42b90d9e636
Saswat Levin <saswatlevin@gmail.com>
2019-07-08 09:36:49 +0300

    add is a filter. include,for,if,else,elif,endfor,endblock,center,upper,firstcap,slugify,safe are all built in template tags in django

--- trydjango/src/pages/views.py
+++ trydjango/src/pages/views.py
@@ -12,11 +12,13 @@ def contact_view(request,*args,**kwargs):
 def about_view(request,*args,**kwargs):
    my_context={
    "my_text":"This is about us",
+    "this_is_true":True,
     "my_number":123,
-    "my_list":[123,4242,319]
+    "my_list":[1313,4231,312,"Abc"]
+
    }
    
- 
+
    return render(request,"about.html",my_context)
 
 def social_view(request,*args,**kwargs):
