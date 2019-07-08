1d1fc27a8c703a5c9b1b59ad5f1415d0e3f4d827
Saswat Levin <saswatlevin@gmail.com>
2019-07-08 09:59:04 +0300

    rendered data on page from model in database

--- trydjango/src/trydjango/urls.py
+++ trydjango/src/trydjango/urls.py
@@ -20,12 +20,13 @@ from pages.views import home_view
 from pages.views import contact_view
 from pages.views import about_view
 from pages.views import social_view
-
+from products.views import product_detail_view
 urlpatterns = [
     path('', home_view, name='home'),
     path('contact/',contact_view,name='contact'),
     path('about/',about_view,name='about'),
     path('social/',social_view,name='social'),
+    path('product/',product_detail_view),
     path('admin/', admin.site.urls)
 
 ]
